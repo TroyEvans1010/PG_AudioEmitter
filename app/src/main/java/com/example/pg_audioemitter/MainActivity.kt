3863a898bb9039234e8efe918945ddeaacd84114
Troy <troye668@gmail.com>
2021-01-09 14:50:42 -0600

    trying to concatenate ByteStrings. Not working, but printing sounds

--- app/src/main/java/com/example/pg_audioemitter/MainActivity.kt
+++ app/src/main/java/com/example/pg_audioemitter/MainActivity.kt
@@ -5,12 +5,18 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.protobuf.ByteString
+import com.tminus1010.tmcommonkotlin.logz.logz
+import com.tminus1010.tmcommonkotlin_rx.observe
+import io.reactivex.rxjava3.core.Observable
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     val audioEmitter by lazy { AudioEmitter() }
     val playMusicUtil by lazy { PlayMusicUtil() }
-    var collected = ByteString.EMPTY
+    var collected = arrayListOf<ByteString>()
+    val collectedByteString
+        get() = ByteString.copyFrom(collected)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,13 +33,21 @@ class MainActivity : AppCompatActivity() {
             }
             // # Start Recording
             audioEmitter.start {
-                collected = collected.concat(it)
-                collected.logx("aaa")
+                it.logx("aaa")
+                collected.add(it)
+//                collected.logx("bbb")
             }
+            // # Stop after some time
+            Observable.just(Unit)
+                .delay(5, TimeUnit.SECONDS)
+                .observe(this) {
+                    audioEmitter.stop()
+                    logz("audioEmitter.stop(). size=${collectedByteString.size()} collectedByteString:$collectedByteString")
+                }
         }
         btn_2.setOnClickListener {
             // # Play Audio
-            playMusicUtil.playMP3ByteArray(this, collected.toByteArray())
+            playMusicUtil.playMP3ByteArray(this, collectedByteString.toByteArray())
         }
     }
 }
