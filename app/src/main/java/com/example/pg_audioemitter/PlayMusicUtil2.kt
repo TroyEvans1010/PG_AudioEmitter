34338cb8ea21187982dfca45be1782b1e5535210
Troy <troye668@gmail.com>
2021-01-10 16:49:49 -0600

    PlayMusicUtil2`not using deprecated constructor

--- app/src/main/java/com/example/pg_audioemitter/PlayMusicUtil2.kt
+++ app/src/main/java/com/example/pg_audioemitter/PlayMusicUtil2.kt
@@ -1,7 +1,6 @@
 package com.example.pg_audioemitter
 
 import android.media.*
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import java.io.*
@@ -9,18 +8,30 @@ import java.io.*
 
 class PlayMusicUtil2 {
     fun play(tempFile: File) {
-        val minBufferSize: Int = AudioTrack.getMinBufferSize(
-            FREQUENCY,
-            AudioFormat.CHANNEL_OUT_MONO,
-            AudioFormat.ENCODING_PCM_16BIT
-        )
-        val audioTrack = AudioTrack(
-            AudioManager.STREAM_MUSIC,FREQUENCY,
-            AudioFormat.CHANNEL_OUT_MONO,AUDIO_FORMAT,
-            minBufferSize,
-            AudioTrack.MODE_STREAM
-        )
-        // * AudioTrack.MODE_STREAM means that play() simply opens the stream.
+        val minBufferSize =
+            AudioTrack.getMinBufferSize(
+                FREQUENCY,
+                AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT
+            )
+        val audioAttributes =
+            AudioAttributes.Builder()
+                .setLegacyStreamType(AudioManager.STREAM_MUSIC)
+                .build()
+        val audioFormat =
+            AudioFormat.Builder()
+                .setChannelMask(AudioFormat.CHANNEL_OUT_MONO)
+                .setSampleRate(FREQUENCY)
+                .setEncoding(AUDIO_FORMAT)
+                .build()
+        val audioTrack =
+            AudioTrack.Builder()
+                .setAudioFormat(audioFormat)
+                .setAudioAttributes(audioAttributes)
+                .setBufferSizeInBytes(minBufferSize)
+                .setTransferMode(AudioTrack.MODE_STREAM)
+                .build()
+        // * MODE_STREAM means that play() does not play, it simply opens the stream.
         audioTrack.play()
 
         val inputStream = FileInputStream(tempFile)
