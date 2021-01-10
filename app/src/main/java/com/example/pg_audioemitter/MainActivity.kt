255ea7c11da4525f7e0ce41a21c03146036be23a
Troy <troye668@gmail.com>
2021-01-10 15:06:25 -0600

    MainActivity`trying to understand header of record old way

--- app/src/main/java/com/example/pg_audioemitter/MainActivity.kt
+++ app/src/main/java/com/example/pg_audioemitter/MainActivity.kt
@@ -3,20 +3,15 @@ package com.example.pg_audioemitter
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.FileUtils
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.example.pg_audioemitter.extensions.toByteArray
-import com.example.pg_audioemitter.extensions.toDisplayStr
-import com.example.pg_audioemitter.extensions.toastAndLog
+import com.example.pg_audioemitter.extensions.*
 import com.example.pg_audioemitter.model_app.AudioEmitterResult
-import com.example.pg_audioemitter.model_app.AudioHeader
 import com.tminus1010.tmcommonkotlin.logz.logz
 import com.tminus1010.tmcommonkotlin.misc.toast
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
-import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.util.concurrent.TimeUnit
 
@@ -29,6 +24,12 @@ class MainActivity : AppCompatActivity() {
         File.createTempFile("kurchina", "mp3", cacheDir)
             .apply { deleteOnExit() }
             .logx("aaa")
+    }
+
+    val tempFile by lazy {
+        File.createTempFile("rtyerty", "file", cacheDir)
+            .apply { deleteOnExit() }
+            .logx("ppp")
     }
 
 
@@ -65,7 +66,7 @@ class MainActivity : AppCompatActivity() {
                 })
                 {
                     toast("Recording encountered error")
-                    Log.e("TMLog","TM`Recording encountered error:", it)
+                    Log.e("TMLog", "TM`Recording encountered error:", it)
                 }
 
         }
@@ -94,10 +95,10 @@ class MainActivity : AppCompatActivity() {
                             toast("Successful recording complete")
                             logz("Successful recording complete. Combined:${it.combinedByteString.toDisplayStr()}")
                             tempMp3.writeBytes(it.combinedByteString.toByteArray())
+                            // * I think this^ is not enough - it also needs a header, which idk how to make.
                             val header = it.combinedByteString.toByteArray().take(44)
                             logz("header:$header")
 
-                            // * I think this^ is not enough - it also needs a header, which idk how to make.
                             btn_2.isEnabled = true
                         }
                         is AudioEmitterResult.AudioChunk -> {
@@ -107,33 +108,51 @@ class MainActivity : AppCompatActivity() {
                 })
                 {
                     toast("Recording encountered error")
-                    Log.e("TMLog","TM`Recording encountered error:", it)
+                    Log.e("TMLog", "TM`Recording encountered error:", it)
                 }
         }
         btn_2.setOnClickListener {
             // # Play Audio
             playMusicUtil.playObservable(tempMp3)
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ toastAndLog("Successful play complete")})
+                .subscribe({ toastAndLog("Successful play complete") })
                 {
                     toast("Play encountered error")
-                    Log.e("TMLog","TM`Play encountered error:", it)
+                    Log.e("TMLog", "TM`Play encountered error:", it)
                 }
         }
         btn_3.setOnClickListener {
             toastAndLog("HasMic: ${hasMicrophone()}")
         }
+        // # Print Something
         btn_4.setOnClickListener {
-            val byteArray = tempMp3.toByteArray()
-            val header = byteArray.take(44)
-            val headerPlus = byteArray.take(440)
-            val lastBit = byteArray.takeLast(44)
-            logz("header:$header")
-            logz("headerPlus:$headerPlus")
-            logz("lastBit:$lastBit")
+            // # Byte.toBitStr()
+            17.toByte().toBitStr().logx("yyy")
+            1010.toByte().toBitStr().logx("iii")
+            64.toByte().toBitStr().logx("uuu")
 
-//            logz(FileInputStream(tempMp3)  .bufferedReader().readLine())
-//            logz(FileInputStream(tempMp3).bufferedReader().readLine())
+            // # header
+//            val byteArray = tempMp3.toByteArray()
+//            val header = byteArray.take(44)
+//            val headerPlus = byteArray.take(440)
+//            val lastBit = byteArray.takeLast(44)
+//            logz("header:$header")
+//            logz("headerPlus:$headerPlus")
+//            logz("lastBit:$lastBit")
+        }
+        btn_5.setOnClickListener {
+            val byteArray = byteArrayOf(
+                'd'.toByte(),
+                'a'.toByte(),
+                't'.toByte(),
+                'a'.toByte()
+            )
+            tempFile.writeBytes(byteArray)
+            Thread.sleep(1000)
+            tempFile.toByteArray().toSpecialStr().logx("ttt")
+        }
+        btn_6.setOnClickListener {
+            tempMp3.toByteArray().toSpecialStr().logx("rrr")
         }
     }
 
--- app/src/main/java/com/example/pg_audioemitter/extensions/ByteArray.kt
+++ app/src/main/java/com/example/pg_audioemitter/extensions/ByteArray.kt
@@ -1,5 +1,9 @@
 package com.example.pg_audioemitter.extensions
 
 fun ByteArray.toSpecialStr(): String {
-    return this.take(44).map { it.toChar() }.joinToString("")
+    return this.take(202).withIndex().map { (k, v) -> "$k:${v.toChar()}," }.joinToString("")
 }
+
+//fun ByteArray.toSpecialStr2(): String {
+//    return this.take(202).withIndex().map { (k, v) -> "$k:${v.toInt().toBin}," }.joinToString("")
+//}
