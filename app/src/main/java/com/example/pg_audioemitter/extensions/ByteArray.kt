e4c352ae09a08ea90b73d132b0739650662e59fc
Troy Swanson <troy.evans@gm.com>
2021-01-11 21:53:46 -0600

    extensions`ByteArray

--- app/src/main/java/com/example/pg_audioemitter/extensions/ByteArray.kt
+++ app/src/main/java/com/example/pg_audioemitter/extensions/ByteArray.kt
@@ -1,5 +1,7 @@
 package com.example.pg_audioemitter.extensions
 
+import com.google.protobuf.ByteString
+
 fun ByteArray.toSpecialStr(): String {
     return this.take(202).withIndex().map { (k, v) -> "$k:${v.toChar()}," }.joinToString("")
 }
@@ -7,3 +9,7 @@ fun ByteArray.toSpecialStr(): String {
 //fun ByteArray.toSpecialStr2(): String {
 //    return this.take(202).withIndex().map { (k, v) -> "$k:${v.toInt().toBin}," }.joinToString("")
 //}
+
+fun ByteArray.toDisplayStr(): String {
+    return this.map { it.toString() }.joinToString(",")
+}
