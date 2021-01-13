a8cede0828ecdff1c67ea935d3ab690de1fa5a6c
Troy Swanson <troy.evans@gm.com>
2021-01-13 00:03:11 -0600

    MainActivity`sampleRate = 8000

--- app/src/main/java/com/example/pg_audioemitter/MainActivity.kt
+++ app/src/main/java/com/example/pg_audioemitter/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
     val mediaRecorderHelper by lazy { MediaRecorderHelper() }
     val partialAudioFormat by lazy {
         PartialAudioFormat(
-            sampleRate = 44100,
+            sampleRate = 8000,
             encoding = AudioFormat.ENCODING_PCM_16BIT,
         )
     }
