0587b270794b7ad652e12c20350300748215f4ae
Troy Swanson <troy.evans@gm.com>
2021-02-23 21:11:13 -0600

    ByteArray`formatting

--- app/src/main/java/com/example/pg_audioemitter/extensions/ByteArray.kt
+++ app/src/main/java/com/example/pg_audioemitter/extensions/ByteArray.kt
@@ -2,10 +2,5 @@ package com.example.pg_audioemitter.extensions
 
 import com.google.protobuf.ByteString
 
-fun ByteArray.toLogStr(): String {
-    return this.map { it.toString() }.joinToString(",")
-}
-
-fun ByteArray.toByteString(): ByteString {
-    return ByteString.copyFrom(this)
-}
+fun ByteArray.toLogStr() = this.map { it.toString() }.joinToString(",")
+fun ByteArray.toByteString() = ByteString.copyFrom(this)
