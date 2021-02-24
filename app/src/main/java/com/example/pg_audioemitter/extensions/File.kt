c87d9ea3deb0941ae896be6e63d3ca1dd021cb45
Troy Swanson <troy.evans@gm.com>
2021-02-23 21:07:44 -0600

    formatting

--- app/src/main/java/com/example/pg_audioemitter/extensions/File.kt
+++ app/src/main/java/com/example/pg_audioemitter/extensions/File.kt
@@ -2,6 +2,4 @@ package com.example.pg_audioemitter.extensions
 
 import java.io.File
 
-fun File.toByteArray(): ByteArray {
-    return org.apache.commons.io.FileUtils.readFileToByteArray(this)
-}
+fun File.toByteArray() = org.apache.commons.io.FileUtils.readFileToByteArray(this)
