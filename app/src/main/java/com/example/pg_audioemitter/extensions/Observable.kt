8cdaa954c622b5ae77efb327c70d8b2fd07985d9
Troy Swanson <troy.evans@gm.com>
2021-02-23 21:07:05 -0600

    Observable`cleanup, endWithItem, emitIfTimeout

--- app/src/main/java/com/example/pg_audioemitter/extensions/Observable.kt
+++ app/src/main/java/com/example/pg_audioemitter/extensions/Observable.kt
@@ -1,11 +1,16 @@
 package com.example.pg_audioemitter.extensions
 
-import android.util.Log
-import com.tminus1010.tmcommonkotlin.logz.logz
 import io.reactivex.rxjava3.core.Observable
+import java.util.concurrent.TimeUnit
 
-fun <T> Observable<T>.logc(prefix: Any?): Observable<T> {
-    return this
-        .doOnNext { logz("$prefix`$it") }
-        .doOnError { Log.d("TMLog","TM`Failure:", it) }
-}
+fun <T> Observable<T>.endWithItem(item: T): Observable<T> =
+    this
+        .concatWith(Observable.just(item))
+
+fun <IN, OUT> Observable<IN>.emitIfTimeout(timespan: Long, timeUnit: TimeUnit, item: OUT): Observable<OUT> =
+    this
+        .take(1)
+        .map { false }
+        .timeout(timespan, timeUnit, Observable.just(true))
+        .filter { it }
+        .map { item }
