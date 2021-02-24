2817772f35615c3db299cbdf9a060536cddba47b
Troy Swanson <troy.evans@gm.com>
2021-02-23 21:13:13 -0600

    Observable.toBehaviorSubject

--- app/src/main/java/com/example/pg_audioemitter/extensions/Observable.kt
+++ app/src/main/java/com/example/pg_audioemitter/extensions/Observable.kt
@@ -1,6 +1,7 @@
 package com.example.pg_audioemitter.extensions
 
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.subjects.BehaviorSubject
 import java.util.concurrent.TimeUnit
 
 fun <T> Observable<T>.endWithItem(item: T): Observable<T> =
@@ -14,3 +15,14 @@ fun <IN, OUT> Observable<IN>.emitIfTimeout(timespan: Long, timeUnit: TimeUnit, i
         .timeout(timespan, timeUnit, Observable.just(true))
         .filter { it }
         .map { item }
+
+fun <T> Observable<T>.toBehaviorSubject(): BehaviorSubject<T> {
+    return BehaviorSubject.create<T>()
+        .also { bs ->
+            // Direct subscription (ie: this.subscribe(behaviorSubject)) is ignored by behaviorSubject.value.
+            this.subscribe(
+                { bs.onNext(it) },
+                { bs.onError(it) },
+                { bs.onComplete() })
+        }
+}
