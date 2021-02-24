9bad07572eb38d98e876cc9584012b85cf9ec7e5
Troy Swanson <troy.evans@gm.com>
2021-02-23 20:01:09 -0600

    cleanup

--- app/src/main/java/com/example/pg_audioemitter/extensions/Byte.kt
+++
@@ -1,9 +0,0 @@
-package com.example.pg_audioemitter.extensions
-
-@ExperimentalUnsignedTypes
-fun Byte.toBitStr(): String {
-    return this
-        .toUByte()
-        .toString(2)
-        .let { "0".repeat(8 - it.length) + it }
-}
--- app/src/main/java/com/example/pg_audioemitter/extensions/Iterable.kt
+++
@@ -1,7 +0,0 @@
-package com.example.pg_audioemitter.extensions
-
-import com.google.protobuf.ByteString
-
-fun <T: ByteString> Iterable<T>.toByteString(): ByteString {
-    return ByteString.copyFrom(this)
-}
