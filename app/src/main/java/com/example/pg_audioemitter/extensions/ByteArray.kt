1d1f43c844708ec327d172d81f57a82dc86f5ecc
Troy Swanson <troy.evans@gm.com>
2021-02-23 21:11:32 -0600

    extensions:ByteArray`toTempFile

--- app/src/main/java/com/example/pg_audioemitter/extensions/ByteArray.kt
+++ app/src/main/java/com/example/pg_audioemitter/extensions/ByteArray.kt
@@ -1,6 +1,17 @@
 package com.example.pg_audioemitter.extensions
 
 import com.google.protobuf.ByteString
+import java.io.File
+import java.io.FileOutputStream
+import java.util.*
 
 fun ByteArray.toLogStr() = this.map { it.toString() }.joinToString(",")
 fun ByteArray.toByteString() = ByteString.copyFrom(this)
+fun ByteArray.toTempFile(cacheDir: File): File =
+    File.createTempFile(UUID.randomUUID().toString().take(15), ".file", cacheDir)
+        .apply { deleteOnExit() }
+        .also {
+            FileOutputStream(it)
+                .apply { write(this@toTempFile) }
+                .apply { close() }
+        }
