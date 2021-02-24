a99bd7856a4d8f8b626f9f6a053d7e9a5ee2652f
Troy Swanson <troy.evans@gm.com>
2021-02-23 21:04:29 -0600

    formatting

--- app/src/main/java/com/example/pg_audioemitter/extensions/Context.kt
+++ app/src/main/java/com/example/pg_audioemitter/extensions/Context.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.widget.Toast
 import com.tminus1010.tmcommonkotlin.logz.logz
 
-fun Context.toastAndLog(msg:String, lengthID:Int= Toast.LENGTH_SHORT) {
+fun Context.toastAndLog(msg: String, lengthID: Int = Toast.LENGTH_SHORT) {
     logz(msg)
     Toast.makeText(this, msg, lengthID).show()
 }
